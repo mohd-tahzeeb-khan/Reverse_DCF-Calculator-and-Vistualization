d17509203805775b66e7afc59c0c0132e4caf8db
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-03-27 03:07:36 +0530

    Functionality of rendering the contain on dropdown menu is ready to use

--- homepage.py
+++ homepage.py
@@ -1,15 +1,16 @@
 from dash import Dash, html, dcc
 import dash
 from dash.dependencies import Output, Input
+from Cal_Graph import calculator
 app = dash.Dash(__name__)
 
-
+global navigationbar
 dropdown=dcc.Dropdown(options=[{
-    "label":dcc.Link(children="Home", href="/github"),
-    "value":"HOmE"
+    "label":dcc.Link(children="Home", href="/"),
+    "value":"HOME"
 
 }, {
-    "label":dcc.Link(children="DCF Valuation", href="/github"),
+    "label":dcc.Link(children="DCF Valuation", href="/DCF"),
     "value":"DCF"
 
 },],placeholder="PAGES",
@@ -27,10 +28,21 @@ navigationbar=html.Div([
 app.layout = html.Div([navigationbar, textline])
 
 @app.callback(
-    Output('dropdownoption', 'value'), # <- dropdown placeholder gets updated
+    Output('dropdownoption', 'placeholder'), # <- dropdown placeholder gets updated
     Input('dropdownoption', 'value'))
 def update_placeholder(value):
     return "PAGES"
 
+
+@app.callback(
+    Output('textline', 'children'),
+    Input('dropdownoption', 'value')
+)
+def render_content(page):
+    print(page)
+    if page=='HOME':
+        return textline
+    else:
+        return calculator
 if __name__ == '__main__':
     app.run(debug=True)
